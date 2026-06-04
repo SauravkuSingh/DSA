6e8ead0b2329770e93c35ee605a4e91db7e520f9
Saurav  Singh <singhsaurav2509@gmail.com>
2026-06-04 23:56:52 +0530

    Add integer reverse utility with overflow checks

---
+++ BasicMaths/reverse.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int reverse(int n) {
+    int rev = 0;
+    while (n != 0) {
+        int lD = n%10;
+        n /= 10;
+        if(rev > INT_MAX/10 || (rev == INT_MAX/10 && lD > 7)) return 0;
+        if(rev < INT_MIN/10 || (rev == INT_MIN/10 && lD < -8)) return 0;
+        rev = rev*10 + lD;
+    }
+    return rev;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << "Reversed number = " << reverse(n) << endl;
+
+    return 0;
+}
